99f6934aa00ab956b1ab00885b3e981f9c028176
Caroline Peri <caroline.anne.peri@gmail.com>
2023-05-26 15:47:05 -0400

    test: add test for AirbenderService  - get_members(nation) method

---
+++ spec/services/airbender_service_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe 'Airbender Service' do
+  it 'returns the Airbender API response' do
+    members = AirbenderService.new.get_members('Fire+Nation')
+    # require 'pry'; binding.pry
+
+    expect(members).to be_an(Array)
+
+    members.each do |member|
+      expect(member).to have_key(:_id)
+      expect(member[:_id]).to be_a(String)
+
+      expect(member).to have_key(:allies)
+      expect(member[:allies]).to be_an(Array)
+
+      expect(member).to have_key(:allies)
+      expect(member[:allies]).to be_an(Array)
+
+      expect(member).to have_key(:name)
+      expect(member[:name]).to be_a(String)
+
+      expect(member).to have_key(:affiliation)
+      expect(member[:affiliation]).to be_a(String)
+    end
+
+    member = members.first
+    expect(member).to have_key(:photoUrl)
+    expect(member[:photoUrl]).to be_a(String)
+  end
+end
