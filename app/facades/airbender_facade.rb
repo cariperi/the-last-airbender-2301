3901e31e86028b7f7bd76a83e1fadf8e2401de51
Caroline Peri <caroline.anne.peri@gmail.com>
2023-05-26 17:00:39 -0400

    refactor: update data formatting to facilitate less conditional logic in the view and tests

--- app/facades/airbender_facade.rb
+++ app/facades/airbender_facade.rb
@@ -1,6 +1,6 @@
 class AirbenderFacade
   def self.get_members(nation)
-    members = AirbenderService.new.get_members(nation)
+    members = service.get_members(nation)
 
     members.map do |member|
       Member.new(clean_member_data(member))
@@ -9,11 +9,15 @@ class AirbenderFacade
 
   private
 
+  def self.service
+    @_service ||= AirbenderService.new
+  end
+
   def self.clean_member_data(member)
     {
       _id: member[:_id],
-      allies: member[:allies],
-      enemies: member[:enemies],
+      allies: member[:allies].empty? ? ["None"] : member[:allies],
+      enemies: member[:enemies].empty? ? ["None"] : member[:enemies],
       photoUrl: member.has_key?(:photoUrl) ? member[:photoUrl] : nil,
       name: member[:name],
       affiliation: member[:affiliation]
