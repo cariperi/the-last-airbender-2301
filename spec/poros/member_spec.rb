2d5d33228243bfe0e40bc9a78d9680ba3fb5c360
Caroline Peri <caroline.anne.peri@gmail.com>
2023-05-26 16:05:29 -0400

    test: update member poro spec to fix expectation errors and setup issue

--- spec/poros/member_spec.rb
+++ spec/poros/member_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Member do
   before(:each) do
     data = {
-      id: "5cf5679a915ecad153ab68cc",
+      _id: "5cf5679a915ecad153ab68cc",
       allies: ["Fire Nation"],
       enemies: ["Aang"],
       photoUrl: "https://vignette.wikia.nocookie.net/avatar/images/2/24/Afiko.png/revision/latest?cb=20121121024128",
@@ -11,17 +11,17 @@ RSpec.describe Member do
       affiliation: "Fire Nation"
     }
 
-    member = Member.new(data)
+    @member = Member.new(data)
   end
 
   it 'exists and has attributes' do
-    expect(member).to be_a(Member)
+    expect(@member).to be_a(Member)
 
-    expect(member.id).to eq("5cf5679a915ecad153ab68cc")
-    expect(member.allies).to eq(["Fire Nation"])
-    expect(member.enemies).to eq(["Aang"])
-    expect(member.photoUrl).to eq(["https://vignette.wikia.nocookie.net/avatar/images/2/24/Afiko.png/revision/latest?cb=20121121024128"])
-    expect(member.name).to eq("Afiko")
-    expect(member.affiliation).to eq("Fire Nation")
+    expect(@member.id).to eq("5cf5679a915ecad153ab68cc")
+    expect(@member.allies).to eq(["Fire Nation"])
+    expect(@member.enemies).to eq(["Aang"])
+    expect(@member.photo).to eq("https://vignette.wikia.nocookie.net/avatar/images/2/24/Afiko.png/revision/latest?cb=20121121024128")
+    expect(@member.name).to eq("Afiko")
+    expect(@member.affiliation).to eq("Fire Nation")
   end
 end
